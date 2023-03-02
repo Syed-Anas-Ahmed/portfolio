1d711c6a25217c31c898d32c9ca2955d6346c769
Syed Anas Ahmed <muhananas@gmail.com>
2023-03-02 11:46:06 +0500

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -29,10 +29,10 @@ const Hero = () => {
           variants={fadeIn("up", "tween", 0.3, 1)}
           className={css.person}
         >
-          <motion.img style={{width:500}} variants={slideIn("up", "tween", 0.5, 1.3)} src="./person.png" alt="" />
+          <motion.img style={{width: auto}} variants={slideIn("up", "tween", 0.5, 1.3)} src="./person.png" alt="" />
         </motion.div>
 
-        <a className={css.email} href="mailto:zainkeepscode@gmail.com">
+        <a className={css.email} href="mailto:engr.doniamandour@gmail.com">
           engr.doniamandour@gmail.com
         </a>
 
